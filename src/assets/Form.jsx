8846385a64731d21afc072f9eb93f5a449e0067c
Chirawattt <chirawat.yana@hotmail.com>
2024-03-06 09:35:16 +0700

    Refactor Form and ShowName components

--- src/assets/Form.jsx
+++ src/assets/Form.jsx
@@ -3,15 +3,15 @@ import ShowName from './ShowName';
 
 export default function Form() {
     const [name, setName] = useState('');
-    const [displayName, setDisplayName] = useState(false);
+    const [displayName, setDisplayName] = useState(false); // name the we input in the form will be displayed if it has 5 or more characters
 
     const handleChange = (e) => {
         console.log(e);
-        setName(e.target.value);
+        setName(e.target.value); // setName value to name variable
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault(); // set the default action of the form to be prevented
         alert('A name was submitted: ' + name);
         console.log('A name was submitted: ' + name);
 
@@ -24,7 +24,7 @@ export default function Form() {
             <form>
                 <label>
                     Name:
-                    <input type="text" value={name} onChange={handleChange} />
+                    <input type="text" value={name} onChange={handleChange} /> 
                 </label>
                 <input type="submit" value="Submit" onClick={handleSubmit} />
             </form>
--- src/assets/ShowName.jsx
+++ src/assets/ShowName.jsx
@@ -1,10 +1,10 @@
 import propTypes from 'prop-types'; // manage or control parameter
 
-export default function ShowName({ name, show }) {
+export default function ShowName({ name, show }) { // name is string and show is boolean that we input from Form.jsx
     console.log("name:",name);
     console.log("show:",show);
 
-    return <div>{show && <h2>Your name is: {name}</h2>}</div>
+    return <div>{show && <h2>Your name is: {name}</h2>}</div> // if show is true, then display the name
 }
 
 ShowName.propTypes = {
